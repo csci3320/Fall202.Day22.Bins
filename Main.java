a3a8b2054f7454b7067d7b32e3b1e0f13349e9ce
UNO User <unouser@unomaha.edu>
2020-10-16 13:21:41 -0500

    Add Timing

--- Main.java
+++ Main.java
@@ -1,41 +1,44 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         new Main();
     }
 
-    public Main(){
+    public Main() {
+        
+        int stop = 100000;
+        long startTime;
+        long endTime;
+
+        startTime = System.nanoTime();
+        
         BinInterface bin = new Bin(100);
+        for (int i = 0; i < stop; i++) {
+            bin.add(14);
+            bin.add(22);
+            bin.remove(22);
+            bin.add(38);
+        }
 
-        bin.add(14);
-        bin.add(22);
-        bin.add(38);
 
-        System.out.println(bin.size());
-        System.out.println(bin.isEmpty());
-        System.out.println(bin.contains(14));
-        System.out.println(bin.contains(15));
-        System.out.println(bin.count(22));
+        endTime = System.nanoTime();
 
-        bin.remove(22);
+        System.out.println((endTime - startTime) / 1000000000.0);
 
-        System.out.println(bin.count(22));
+        startTime = System.nanoTime();
+        
+        bin = new BinArrayList();
+        for (int i = 0; i < stop; i++) {
+            bin.add(14);
+            bin.add(22);
+            bin.remove(22);
+            bin.add(38);
+        }
 
-        BinInterface binList = new BinArrayList();
 
-        binList.add(14);
-        binList.add(22);
-        binList.add(38);
+        endTime = System.nanoTime();
 
-        System.out.println(binList.size());
-        System.out.println(binList.isEmpty());
-        System.out.println(binList.contains(14));
-        System.out.println(binList.contains(15));
-        System.out.println(binList.count(22));
+        System.out.println((endTime - startTime) / 1000000000.0);
 
-        binList.remove(22);
-
-        System.out.println(binList.count(22));
     }
-
 
 }
